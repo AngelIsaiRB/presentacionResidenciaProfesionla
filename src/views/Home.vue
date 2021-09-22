874fbf44830bd20ebce9f3fd695643c8a87a6a58
AngelIsaiRB <qwerasdffdeswa@live.com>
2021-09-21 21:37:39 -0500

    final not pdf

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,235 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class=" h-screen w-screen fondo">
+        <div class="h-2/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/tec.png" alt="" />
+      </div>
+    </div>
+    <div class="flex h-2/6 justify-center animate__animated animate__fadeIn text-center">
+      <div>
+        <p class="animate__animated animate__fadeIn text-4xl">INSTITUTO TECNOLÓGICO DEL VALLE DE OAXACA</p>
+        <br />
+        <p class="animate__animated animate__fadeIn text-2xl">
+          PROYECTO DE RESIDENCIA PROFESIONAL
+        </p>
+        <p class="animate__animated animate__fadeIn text-3xl uppercase">
+          nombre del proyecto
+        </p>
+        <br />
+        <p class="animate__animated animate__fadeIn text-3xl">PRESENTA</p>
+        <br />
+        <p class="animate__animated animate__fadeIn text-3xl ">ANGEL ISAI RAMIREZ BAZAN</p>
+        <br /><br /><br />
+        <p class="animate__animated animate__fadeIn text-3xl ">
+          INGENIERÍA EN TECNOLOGÍAS DE LA INFORMACIÓN Y COMUNICACIONES
+        </p>
+      </div>
+    </div>
+    <div class="flex justify-end h-2/6 pb-12 pr-12">
+      <div class="flex items-end">
+        <p class="font-bold text-3xl">
+          EX HACIENDA DE NAZARENO, XOXOCOTLÁN, OAXACA. SEPTIEMBRE 2021
+        </p>
+      </div>
+      </div>
   </div>
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-2/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-2/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-bold">Problematica: </p>
+       <div class=" h-2 bg-black w-full"></div>
+       <br>
+      <p class="text-4xl leading-relaxed">El Instituto Tecnológico del Valle de Oaxaca a través del departamento de 
+ciencias económico administrativo lleva la actividad de generar el seguimiento 
+del proceso de residencia profesional de las carreras de ingeniería en informática e 
+ingeniería en tecnologías de la información y comunicaciones, actualmente se 
+lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas</p>
+    </div>
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-2/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-2/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-bold uppercase">Objetivo general: </p>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+      <p class="text-5xl leading-relaxed">Implementar una plataforma web para la administracion y seguimiento de Residencias profesionales</p>
+    </div>
+    </div>
+    <div class="flex h-2/6 max-w-full justify-around">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/web.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/een.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/build.jpg" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-2/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-bold uppercase">Marco de Trabajo: </p>
+      <br>
+      <p class="text-6xl font-semibold uppercase">Scrum</p>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+      <p class="text-5xl leading-relaxed">A través de una metodología como SCRUM, se logra de manera exitosa la integración de todas las partes involucradas en un proyecto. La participación y administración de SCRUM es sencilla y de fácil manejo para todas las etapas. </p>
+    </div>
+    </div>
+    <div class="flex h-3/6 max-w-full justify-around">
+      <img class="w-full animate__animated animate__fadeIn object-scale-down" src="../assets/scrum.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Herramientas: </p>
+      <br>
+      <p class="text-6xl font-bold uppercase">Diseño: </p>
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+      <p class="text-3xl leading-relaxed">Para diseñar la plataforma se utilizo: </p>
+    </div>
+    </div>
+    <div class="flex h-3/6 max-w-full justify-around">
+      <img class="w-3/6 animate__animated animate__fadeIn object-scale-down" src="../assets/start.png" alt="">
+      <img class="w-3/6 animate__animated animate__fadeIn object-scale-down" src="../assets/figma.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class=" w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Herramientas: </p>
+      <br>
+      <p class="text-6xl font-bold uppercase">Desarollo: </p>
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+      <p class="text-3xl leading-relaxed">Para el desarollo la plataforma se utilizo: </p>
+    </div>
+    </div>
+    <div class="flex h-4/6 max-w-full justify-around">
+      <div class="w-11/12">
+        <div class="flex max-w-full justify-around my-16">
+        <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/javas.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/html.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/css.png" alt="">
+      </div>
+        <div class="flex max-w-full justify-around my-16">
+        <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/vscode.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/tailwind.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/npm.jpeg" alt="">
+      </div>
+      <div class="flex max-w-full justify-around my-16">
+        <img class="w-4/6 animate__animated animate__fadeIn object-scale-down" src="../assets/vueop.png" alt="">
+      <!-- <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/figma.png" alt=""> -->
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/git.png" alt="">
+      </div>
+      <div class="flex max-w-full justify-around my-16">
+        <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/github.png" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/heroku.jpg" alt="">
+      <img class="w-2/6 animate__animated animate__fadeIn object-scale-down" src="../assets/netlify.jpeg" alt="">
+      </div>
+      <div class="flex max-w-full justify-around my-16">
+        <img class="w-3/6 animate__animated animate__fadeIn object-scale-down" src="../assets/firebase.png" alt="">
+      <img class="w-3/6 animate__animated animate__fadeIn object-scale-down" src="../assets/firebaseutilities.png" alt="">
+      </div>
+      </div>
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados </p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full">
+    <div class="flex justify-center h-full ">
+       <iframe class="w-11/12  h-full" src="https://testing-itvo-isai.netlify.app" frameborder="0"></iframe>
+    </div>
+    <div class="h-80">
+
+    </div>
+    </div>
+  </div>
+  <!--  -->
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
-}
+  name: "Home",
+  components: {},
+};
 </script>
+<style >
+.fondo{
+  background-image: url("../assets/fondo.png");
+  object-fit: contain;
+  background-size: cover;
+  background-repeat:no-repeat;
+  background-position: center center;
+}
+</style>
