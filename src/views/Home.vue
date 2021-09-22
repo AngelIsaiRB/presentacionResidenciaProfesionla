d4603dc7077b2d37b7eb598de906f96558059ee5
AngelIsaiRB <qwerasdffdeswa@live.com>
2021-09-21 22:01:37 -0500

    final

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -10,7 +10,7 @@
     </div>
     <div class="flex h-2/6 justify-center animate__animated animate__fadeIn text-center">
       <div>
-        <p class="animate__animated animate__fadeIn text-4xl">INSTITUTO TECNOLÓGICO DEL VALLE DE OAXACA</p>
+        <p class="animate__animated animate__fadeIn text-2xl">INSTITUTO TECNOLÓGICO DEL VALLE DE OAXACA</p>
         <br />
         <p class="animate__animated animate__fadeIn text-2xl">
           PROYECTO DE RESIDENCIA PROFESIONAL
@@ -30,7 +30,7 @@
     </div>
     <div class="flex justify-end h-2/6 pb-12 pr-12">
       <div class="flex items-end">
-        <p class="font-bold text-3xl">
+        <p class="font-bold text-xl">
           EX HACIENDA DE NAZARENO, XOXOCOTLÁN, OAXACA. SEPTIEMBRE 2021
         </p>
       </div>
@@ -51,7 +51,7 @@
       <p class="text-6xl font-bold">Problematica: </p>
        <div class=" h-2 bg-black w-full"></div>
        <br>
-      <p class="text-4xl leading-relaxed">El Instituto Tecnológico del Valle de Oaxaca a través del departamento de 
+      <p class="text-2xl leading-relaxed">El Instituto Tecnológico del Valle de Oaxaca a través del departamento de 
 ciencias económico administrativo lleva la actividad de generar el seguimiento 
 del proceso de residencia profesional de las carreras de ingeniería en informática e 
 ingeniería en tecnologías de la información y comunicaciones, actualmente se 
@@ -75,7 +75,7 @@ lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas
       <p class="text-6xl font-bold uppercase">Objetivo general: </p>
       <div class=" h-2 bg-black w-full"></div>
       <br>
-      <p class="text-5xl leading-relaxed">Implementar una plataforma web para la administracion y seguimiento de Residencias profesionales</p>
+      <p class="text-3xl leading-relaxed">Implementar una plataforma web para la administracion y seguimiento de Residencias profesionales</p>
     </div>
     </div>
     <div class="flex h-2/6 max-w-full justify-around">
@@ -102,7 +102,7 @@ lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas
       <p class="text-6xl font-semibold uppercase">Scrum</p>
       <div class=" h-2 bg-black w-full"></div>
       <br>
-      <p class="text-5xl leading-relaxed">A través de una metodología como SCRUM, se logra de manera exitosa la integración de todas las partes involucradas en un proyecto. La participación y administración de SCRUM es sencilla y de fácil manejo para todas las etapas. </p>
+      <p class="text-2xl leading-relaxed">A través de una metodología como SCRUM, se logra de manera exitosa la integración de todas las partes involucradas en un proyecto. La participación y administración de SCRUM es sencilla y de fácil manejo para todas las etapas. </p>
     </div>
     </div>
     <div class="flex h-3/6 max-w-full justify-around">
@@ -187,6 +187,7 @@ lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas
       </div>
     </div>
   </div>
+  
   <!--  -->
   <div class="h-screen w-screen fondo">
     <div class="w-full h-px bg-black"></div>
@@ -200,7 +201,216 @@ lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas
     </div>
     <div class="flex h-1/6 justify-center">
     <div class="w-5/6">
-      <p class="text-6xl font-base uppercase">Resultados </p>            
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/public1.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/admin1.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/admin2.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/admin3.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-4/6 w-11/12 object-scale-down" src="../assets/admin4.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/user1.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/user2.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/user3.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados</p>            
+      <br>
+      <div class=" h-2 bg-black w-full"></div>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full flex justify-center bg-black bg-opacity-30">
+    <img class="h-5/6 w-11/12 object-scale-down" src="../assets/user4.png" alt="">
+    </div>
+  </div>
+  <!--  -->
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase">Resultados Live</p>            
       <br>
       <div class=" h-2 bg-black w-full"></div>
       <br>
@@ -210,8 +420,30 @@ lleva de forma digital pero con uso de hojas de Excel y en ocasiones en libretas
     <div class="flex justify-center h-full ">
        <iframe class="w-11/12  h-full" src="https://testing-itvo-isai.netlify.app" frameborder="0"></iframe>
     </div>
-    <div class="h-80">
-
+    </div>
+  </div>
+  
+    <div class="h-80"></div>
+  <div class="h-screen w-screen fondo">
+    <div class="w-full h-px bg-black"></div>
+ <div class="h-1/6 w-screen flex justify-around">
+      <div class="">
+        <img src="../assets/educacion.png" alt="" />
+      </div>
+      <div class="">
+        <img src="../assets/itvo.png" style="width:140px; heigth:150px" alt="" />
+      </div>
+    </div>
+    <div class="flex h-1/6 justify-center">
+    <div class="w-5/6">
+      <p class="text-6xl font-base uppercase"></p>            
+      <br>
+      <br>
+    </div>
+    </div>
+    <div class=" h-5/6 w-full">
+    <div class="flex justify-center h-full ">
+      <p class="text-6xl">Gracias por su atencion</p>
     </div>
     </div>
   </div>
